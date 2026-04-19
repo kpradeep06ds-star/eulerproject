1873cfefed328efa3c959be0ef262f18cfe16606
Pradeep <kpradeep06.ds@gmail.com>
2026-04-19 09:14:03 +0000

    adding the unique numbers

--- p32/src/main.rs
+++ p32/src/main.rs
@@ -1,17 +1,55 @@
+fn check(a: i32, b: i32) -> (i32, bool) {
+    let fvalue = a * b;
+    let f = format!("{}{}{}", a, b, fvalue);
+
+    // 1. Must be exactly 9 characters
+    // 2. Must NOT contain '0'
+    if f.len() != 9 || f.contains('0') {
+        return (fvalue, false);
+    }
+
+    let mut seen_digits = Vec::new();
+
+    for k in f.chars() {
+        if seen_digits.contains(&k) {
+            return (fvalue, false); // Found a duplicate digit
+        } else {
+            seen_digits.push(k);
+        }
+    }
+    (fvalue, true)
+}
+
 fn main() {
-    // 1x4 = 4 digits
-    // 3x2 or 2x3 = 4 digits 
-    // these are the only scenarios
+    let mut products: Vec<i32> = vec![];
+    let mut temp:(i32, bool) ;
+
     for i in 1..=9{
         for j in 1234..=9876{
-
+            temp = check(i as i32, j as i32);
+            if temp.1 == true{
+                products.push(temp.0);
+            }
         }
     }
 
     for i in 12..=98{
         for j in 123..=987{
-            
+            temp = check(i as i32, j as i32);
+            if temp.1 == true{
+                products.push(temp.0);
+            }
         }
-    }
+    } 
 
+    products.sort();
+    products.dedup(); 
+    
+    let total: i32 = products.iter().sum();
+    
+    if let Some(&first) = products.first() {
+        println!("Total Sum: {}, First Product: {}", total, first);
+    } else {
+        println!("No products found.");
+    }
 }
