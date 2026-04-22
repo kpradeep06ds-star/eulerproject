8dd7172688a9ba92324424809e3a121094ac91e5
Pradeep <kpradeep06.ds@gmail.com>
2026-04-22 16:23:52 +0000

    last 10 numbers via modulo

--- p48/src/main.rs
+++ p48/src/main.rs
@@ -21,7 +21,32 @@
 //     factors
 // }
 
+// fn modby(num:i64, powv:i64) -> i64{
+//     let mut result:i64 = 1;
+
+//     for _i in 1..=pow{
+//         result = (result*num) % 10.pow(powv);
+//     }
+//     result
+// }
+
+fn modby(num: u128, powv: u128) -> u128 {
+    let modulus = 10u128.pow(10); // fixed: last 10 digits
+    let mut result = 1;
+
+    for _ in 0..powv {
+        result = (result * num) % modulus;
+    }
+
+    result
+}
+
 fn main(){
-//
+    let mut sums: u128 = 0;
+    for i in 1..=1000{
+        sums += modby(i as u128, i as u128);
+    }
+    //let result = modby(67, 67) ;
+    println!("{}", sums % 10_u128.pow(10));
  
 }
